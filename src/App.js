fa2db670edab9b639fb729a190d2e6ca9488d901
Annette Tisdale <annette.tisdale@babycenter.com>
2019-11-22 17:03:59 -0800

    edit to app.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+Annette did this she is cool.
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
